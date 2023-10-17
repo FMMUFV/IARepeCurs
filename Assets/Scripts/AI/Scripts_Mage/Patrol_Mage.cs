29bf23a3fb683eb3e1dee1dc4115072be7f6866b
FMMUFV <9004453@alumnos.ufv.es>
2023-10-17 16:57:55 +0200

    Patrol_Mage
    hace la patrulla, pero hay que mirar las que pide el ejercicio y variables velociadad

--- Assets/Scripts/AI/Scripts_Mage/Patrol_Mage.cs
+++ Assets/Scripts/AI/Scripts_Mage/Patrol_Mage.cs
@@ -1,13 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
-using UnityEngine.UI;
 
-public class Mage : MonoBehaviour
+public class Patrol_Mage : StateMachineBehaviour
 {
-    // Start is called before the first frame update
 
     private Transform Destino;//Direccion a la que tiene que ir
     private int siguientePos;
@@ -19,40 +16,56 @@ public class Mage : MonoBehaviour
 
     public List<Transform> ListaWaypoints;
 
-    void Start()
-    {
-        //iniciar();
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-
-        //patrulla();
-
-        //EnPuente();
-    }
-
+    private Mage scritc;
 
     private bool EnlaArena = false;
 
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+
+        //Asignar componencte scrit para obtener sus componentes del scrtip
+        scritc = animator.gameObject.GetComponent<Mage>();
+        ListaWaypoints = scritc.ListaWaypoints;
+
+        //-----Puente recojer velocidad inicial------
+        NavMeshAgent aget = animator.GetComponent<NavMeshAgent>();
+        VelocidadIni = aget.speed;
+
+        //Asignar el primer destino
+        Destino = ListaWaypoints[0];
+
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+
+        patrulla(animator);
+        EnPuente(animator);
+
+
+    }
+
+
+
+    
+
     //Puente O pasarela
-    public void EnPuente()
+    public void EnPuente(Animator animator)
     {
 
 
         PuenteMask = 1 << NavMesh.GetAreaFromName("Scaffold");
         NavMeshHit hit;
-        if (NavMesh.SamplePosition(transform.position, out hit, 2.0f, PuenteMask))
+        if (NavMesh.SamplePosition(animator.transform.position, out hit, 2.0f, PuenteMask))
         {
-           
+
 
             if (EnlaArena == true) //Para que le cambie la velocidad solo una vez cuando este la arena
             {
                 //NavMeshAgent agent = animator.GetComponent<NavMeshAgent>();
-                NavMeshAgent aget = GetComponent<NavMeshAgent>();
+                NavMeshAgent aget = animator.GetComponent<NavMeshAgent>();
                 aget.speed = VelocidadIni / 2;
                 EnlaArena = false;
                 Debug.Log("pisando");
@@ -63,17 +76,17 @@ public class Mage : MonoBehaviour
         }
         else
         {
-            NavMeshAgent aget = GetComponent<NavMeshAgent>();
+            NavMeshAgent aget = animator.GetComponent<NavMeshAgent>();
             aget.speed = VelocidadIni;
             EnlaArena = true;
         }
     }
 
 
-    public void patrulla()
+    public void patrulla(Animator animator)
     {
         //Inicializar y crear variable aget
-        NavMeshAgent aget = GetComponent<NavMeshAgent>();
+        NavMeshAgent aget = animator.GetComponent<NavMeshAgent>();
         //Variable Dist para ver la distancia que hay de su destino
         float dist = Vector3.Distance(Destino.position, aget.transform.position);
         //La dimension que tiene la lista de Waypoints
@@ -99,17 +112,4 @@ public class Mage : MonoBehaviour
         aget.destination = Destino.position;
 
     }
-
-
-    public void iniciar()
-    {
-        //-----Puente recojer velocidad inicial------
-        NavMeshAgent aget = GetComponent<NavMeshAgent>();
-        VelocidadIni = aget.speed;
-
-        //Asignar el primer destino
-        Destino = ListaWaypoints[0];
-    }
-
-
 }
