c87fd5446470334684122f7f739738088d249b73
Fernando Molina <9004453@alumnos.ufv.es>
2023-11-02 16:28:15 +0100

    arreglando cosillas

--- Assets/Scripts/AI/Scritps_Minion/Attack_Minion.cs
+++ Assets/Scripts/AI/Scritps_Minion/Attack_Minion.cs
@@ -112,26 +112,9 @@ public class Attack_Minion : StateMachineBehaviour
             // La cuenta atrás ha llegado a cero, puedes agregar acciones aquí
             AtacaDeNuevo = true;
             currentTime = countdownTime;
-            //animator.SetBool("Patrol", true);
-            animator.SetTrigger("Patrol_1");
+
         }
     }
 
-    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
 
-    // OnStateMove is called right after Animator.OnAnimatorMove()
-    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    // Implement code that processes and affects root motion
-    //}
-
-    // OnStateIK is called right after Animator.OnAnimatorIK()
-    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    // Implement code that sets up animation IK (inverse kinematics)
-    //}
 }
