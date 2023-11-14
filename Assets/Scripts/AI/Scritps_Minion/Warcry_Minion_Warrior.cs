dc00a43b21101411949d5ecb8c89568957640199
Fernando Molina <9004453@alumnos.ufv.es>
2023-11-14 15:15:27 +0100

    el estado de Warcry de minion y Warrior esta todo lo que pide pero falta programar que si no puede por que el nadvmes no ledeja que buelba a patrol

--- Assets/Scripts/AI/Scritps_Minion/Warcry_Minion_Warrior.cs
+++ Assets/Scripts/AI/Scritps_Minion/Warcry_Minion_Warrior.cs
@@ -6,8 +6,9 @@ using UnityEngine.AI;
 public class Warcry_Minion_Warrior : StateMachineBehaviour
 {
     Agent ScritpAgent;
-
-   
+    int DistanUltimaPosJugador = 1;
+    public float raycas;
+    RaycastHit hit;//rayo
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
@@ -22,8 +23,47 @@ public class Warcry_Minion_Warrior : StateMachineBehaviour
         ScritpAgent = animator.GetComponent<Agent>();
         aget.destination = ScritpAgent.UltimaPosicion_Jugador;
 
+        Rayo(animator);
+
+        //Calcular Distanicia de posicion
+        //Si a llegado a su destino cambia a Warcry
+        float distanciaAlJugador = Vector3.Distance(animator.transform.position, ScritpAgent.UltimaPosicion_Jugador);
+
+        if (distanciaAlJugador < DistanUltimaPosJugador)
+        {
+           
+            animator.SetBool("Warcry", false);
+         
+        }
+
     }
 
+
+    public void Rayo(Animator animator)
+    {
+        ScritpAgent = animator.gameObject.GetComponent<Agent>();
+        raycas = ScritpAgent.raycas;
+        // Obtener la dirección del rayo en función de la rotación del animator
+        Vector3 rayDirection = animator.transform.forward;
+
+        // Dibuja un rayo de depuración (para visualización en el Editor de Unity)
+        Debug.DrawRay(animator.transform.position + Vector3.up, animator.transform.forward * raycas, Color.green);
+
+        // Realiza un raycast y almacena la información de colisión en 'hit'
+
+        if (Physics.Raycast(animator.transform.position + Vector3.up, animator.transform.forward, out hit, raycas))
+        {
+            // Comprobar si el objeto golpeado tiene una etiqueta "Player"
+
+            if (hit.transform.gameObject.tag == "Player")
+            {
+                animator.SetBool("Pursue", true);  
+             
+            }
+       
+        }
+    
+    }
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     //{
