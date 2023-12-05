5e421453096ef32147523a0d683d0a16d2d3a2c3
Fernando Molina <9004453@alumnos.ufv.es>
2023-12-05 15:11:01 +0100

    Retoque de script

--- Assets/Scan_Archer.cs
+++ Assets/Scan_Archer.cs
@@ -19,7 +19,7 @@ public class Scan_Archer : StateMachineBehaviour
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-       // Rayo(animator);
+        Rayo(animator);
     }
 
 
@@ -29,28 +29,38 @@ public class Scan_Archer : StateMachineBehaviour
 
     public void Rayo(Animator animator)
     {
+        script.Jugador = GameObject.FindGameObjectWithTag("Player");
+
+
+        // ----------- Desede esta parte se le pasa la ultima posicion del jugador
         Vector3 rayDirection = animator.transform.forward;
-
-
-        Vector3 ultimapos = script.UltimaPosicion_Jugador;
-        Vector3 direccion = ultimapos - animator.transform.position;
+        Vector3 direccion = script.UltimaPosicion_Jugador - animator.transform.position;
         Quaternion rotation = Quaternion.LookRotation(direccion);
 
         rayDirection = rotation * Vector3.forward;
 
+        //----------------------------------------------------------------------------
 
 
+        //------Aqui se le pasa la posicion directamente del jugador
+        Vector3 rayDirection2 = animator.transform.forward;
+        Vector3 direccion2 = script.Jugador.transform.position - animator.transform.position;
+        Quaternion rotation2 = Quaternion.LookRotation(direccion2);
 
+        rayDirection2 = rotation2 * Vector3.forward;
 
+        //----------------
 
-        Debug.DrawRay(animator.transform.position + Vector3.up * 1.5f, rayDirection * raycas, Color.red);
+        Debug.DrawRay(animator.transform.position + Vector3.up * 1.5f, rayDirection2 * raycas, Color.red);
 
-        if (Physics.Raycast(animator.transform.position + Vector3.up*1.5f, rayDirection, out hit, raycas))
+        if (Physics.Raycast(animator.transform.position + Vector3.up*1.5f, rayDirection2, out hit, raycas))
         {
 
             // Detectar al jugador
             if (hit.transform.gameObject.tag == "Player")
             {
+                Debug.Log("El arquero lo ve");
+
                 animator.SetBool("Patrol", false);
                 animator.SetBool("Pursue", true);
 
